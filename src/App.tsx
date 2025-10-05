d031f087afe65c5215aec026fd0cc2ed4fbb3b09
njustinbunos <justinjairebanzuela.bunos25@bicol-u.edu.ph>
2025-10-05 12:51:05 +0800

    feat: render chess Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import './App.css'
+// App.tsx
+import { useChessGame } from './useChessGame';
+import { ChessBoard } from './components/ChessBoard';
+import { GameStatus } from './components/GameStatus';
+import { ResetButton } from './components/ResetButton';
+import './App.css';
 
-function App() {
+export default function App() {
+  const { 
+    chessPosition, 
+    onPieceDrop, 
+    resetGame, 
+    isCheck, 
+    isCheckmate, 
+    isGameOver 
+  } = useChessGame();
+
   return (
-    <>
-    </>
-  )
+    <div className='chessContainer'>
+      <ChessBoard position={chessPosition} onPieceDrop={onPieceDrop} />
+      <GameStatus 
+        isCheck={isCheck} 
+        isCheckmate={isCheckmate} 
+        isGameOver={isGameOver} 
+      />
+      <ResetButton onReset={resetGame} />
+    </div>
+  );
 }
-
-export default App
