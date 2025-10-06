d31106449a7c439661e95cdab9be084cea0eb07c
njustinbunos <justinjairebanzuela.bunos25@bicol-u.edu.ph>
2025-10-06 20:49:23 +0800

    feat: render components

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,39 @@
-// App.tsx
-import { useChessGame } from './useChessGame';
-import { ChessBoard } from './components/ChessBoard';
-import { GameStatus } from './components/GameStatus';
-import { ResetButton } from './components/ResetButton';
+import { Sidebar } from "./components/Sidebar";
+import { Header } from "./components/Header";
+import { ChessBoard } from "./components/ChessBoard";
+import { GameStatus } from "./components/GameStatus";
+import { useChessGame } from "./useChessGame";
+import { ControlPanel } from './components/ControlPanel';
 import './App.css';
 
 export default function App() {
-  const { 
-    chessPosition, 
-    onPieceDrop, 
-    resetGame, 
-    isCheck, 
-    isCheckmate, 
-    isGameOver 
+  const {
+    chessPosition,
+    onPieceDrop,
+    resetGame,
+    isCheck,
+    isCheckmate,
+    isGameOver,
   } = useChessGame();
 
   return (
-    <div className='chessContainer'>
-      <ChessBoard position={chessPosition} onPieceDrop={onPieceDrop} />
-      <GameStatus 
-        isCheck={isCheck} 
-        isCheckmate={isCheckmate} 
-        isGameOver={isGameOver} 
-      />
-      <ResetButton onReset={resetGame} />
+    <div className="layout">
+      <Header />
+      <div className="main-layout">
+        <div className="main-content">
+          <div className="chessBoardContainer">
+            <ChessBoard position={chessPosition} onPieceDrop={onPieceDrop} />
+            <ControlPanel onReset={resetGame} />
+            <GameStatus
+              isCheck={isCheck}
+              isCheckmate={isCheckmate}
+              isGameOver={isGameOver}
+            />
+            
+          </div>
+        </div>
+        <Sidebar />
+      </div>
     </div>
   );
 }
